f9b9d70871dd8a17ad1316cf62bf5c32402b73cd
Yogium <mpp.yoga@gmail.com>
2025-06-17 19:30:34 +0700

    initial commit for LCD GUI elements

---
+++ src/lcd_gui.cpp
@@ -0,0 +1,14 @@
+#include "lcd_gui.h"
+
+//declare TFT_eSPI object
+TFT_eSPI tft = TFT_eSPI(); // Create object "tft"
+
+//function to initialize the TFT display
+void initDisplay() {
+    tft.init(); // Initialize the TFT display
+    tft.setRotation(1); // Set the rotation of the display
+    tft.fillScreen(TFT_BLACK); // Fill the screen with black color
+    tft.setTextColor(TFT_WHITE, TFT_BLACK); // Set text color to white with black background
+    tft.setTextSize(2); // Set text size
+    tft.setCursor(0, 0); // Set cursor position to top left corner
+}
---
+++ src/lcd_gui.h
@@ -0,0 +1,4 @@
+#include <TFT_eSPI.h>
+
+//function to initialize the TFT display
+void initDisplay();
