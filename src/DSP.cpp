8ecc8480afe1bf87b521d55237f6758932ed4cd8
Yogium <mpp.yoga@gmail.com>
2025-06-17 15:35:43 +0700

    fixed FFT Processing in DSP.cpp

--- src/DSP.cpp
+++ src/DSP.cpp
@@ -1,12 +1,12 @@
 #include "DSP.h"
 
 // declaring fft and ADC variables
-arduinoFFT FFT = arduinoFFT();
+float vReal[SAMPLES];
+float vImag[SAMPLES];
+ArduinoFFT<float> FFT = ArduinoFFT<float>(vReal, vImag, SAMPLES, SAMPLING_FREQUENCY);
 unsigned int samplingPeriod; //in us
 unsigned long microseconds;
 
-double vReal[SAMPLES];
-double vImag[SAMPLES];
 
 const int ADC_PIN = 34; //esp32 ADC1 pin
 
@@ -28,20 +28,10 @@ float getFrequency(){
         while (micros() - microseconds < samplingPeriod); //wait for the next sample
     }
     //perform the FFT
-    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD); //apply Hamming window
-    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD); //perform the FFT
-    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES); //convert the complex numbers to magnitude
-
-    //find frequency
-    double maxVal = 0;
-    int maxIndex = 0;
-    for(int i=1; i < (SAMPLES / 2); i++) { //ignores first value (DC component)
-        if(vReal[i] > maxVal) {
-            maxVal = vReal[i];
-            maxIndex = i;
-        }
-    }
-    float frequency = (maxIndex * SAMPLING_FREQUENCY) / SAMPLES; //calculate the frequency
+    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); //apply Hamming window
+    FFT.compute(FFTDirection::Forward); //perform the FFT
+    FFT.complexToMagnitude(); //convert the complex numbers to magnitude
+    float frequency = FFT.majorPeak(); //get the frequency of the major peak
     return frequency; //return the frequency
 }
 
